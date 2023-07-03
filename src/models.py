6bbd2afd856698b763f5c00fc7e190e8f57a18eb
Michel <49735520+michelcub@users.noreply.github.com>
2023-07-03 14:32:48 +0000

    fix models

--- src/models.py
+++ src/models.py
@@ -1,113 +1,86 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
 
 
-class User(Base):
+class User(db.Model):
     __tablename__ = "user"
-    id = Column(Integer, primary_key=True)
-    nickname = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    favorites = relationship('Favorites', backref='user', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
-class Favorites(Base):
+class Favorites(db.Model):
     __tablename__ = "favorites"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user_id = Column(Integer, ForeignKey('planet.id'))
-    user_id = Column(Integer, ForeignKey('character.id'))
-    user_id = Column(Integer, ForeignKey('vehicles.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
 
-class Planet(Base):
+class Planet(db.Model):
     __tablename__ = "planet"
-    id = Column(Integer, primary_key=True)
-    user_id = 
-    name = Column(String, nullable=False, unique=True)
-    url = Column(String, nullable=False, unique=True)
-    favorites = relationship('Favorites', backref='planet', lazy=True)
-    planet_properties = relationship('Planet_Properties', populates="Planet" , lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    climate = db.Column(db.String, nullable=False)
+    created = db.Column(db.String, nullable=False)
+    diameter = db.Column(db.String, nullable=False)
+    films = db.Column(db.String, nullable=False)
+    gravity = db.Column(db.String, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    residents = db.Column(db.String, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    surface_water = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    edited = db.Column(db.DateTime, nullable=False)
+    url = db.Column(db.String, nullable=False, unique=True)
+    favorites = db.relationship('Favorites', backref='planet', lazy=True)
 
-class Planet_Properties(Base):
-    __tablename__ = "planet_properties"
-    id = Column(Integer, primary_key=True)
-    planet_id = Column(ForeignKey('planet.id'))
-    name = Column(ForeignKey('planet.name'))
-    climate = Column(String, nullable=False)
-    created = Column(String, nullable=False)
-    diameter = Column(String, nullable=False)
-    films = Column(String, nullable=False)
-    gravity = Column(String, nullable=False)
-    orbital_period = Column(Integer, nullable=False)
-    population = Column(Integer, nullable=False)
-    residents = Column(String, nullable=False)
-    rotation_period = Column(Integer, nullable=False)
-    surface_water = Column(Integer, nullable=False)
-    terrain = Column(String, nullable=False)
-    created=Column(DateTime, nullable=False)
-    edited= Column(DateTime, nullable=False)
-    url = Column(String, nullable=False, unique=True)
-    planet = relationship('Planet', populates="Planet_Properties" , lazy=True)
-
-
-class Vehicle(Base):
+class Vehicle(db.Model):
     __tablename__ = "vehicle"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False, unique=True)
-    url = Column(String, nullable=False, unique=True)
-    favorites = relationship('Favorites', backref='vehicle', lazy=True) 
-    vehicle_properties = relationship('Vehicle_Properties', populates="Vehicle" , lazy=True) 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    cargo_capacity = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.String, nullable=False)
+    crew = db.Column(db.Integer, nullable=False)
+    length = db.Column(db.String, nullable=False)
+    manufacturer = db.Column(db.String, nullable=False)
+    max_atmosphering_speed = db.Column(db.Integer, nullable=False)
+    model = db.Column(db.String, nullable=False)
+    vehicle_class = db.Column(db.String, nullable=False)
+    passengers = db.Column(db.Integer, nullable=False)
+    pilots = db.Column(db.String, nullable=False)
+    films = db.Column(db.String, nullable=False)
+    url = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    edited = db.Column(db.DateTime, nullable=False)
+    favorites = db.relationship('Favorites', backref='vehicle', lazy=True)
 
-class Vehicle_Properties(Base):
-    __tablename__ = "vehicle_properties"
-    id = Column(Integer, primary_key=True)
-    vehicle_id = Column(ForeignKey('vehicle.id'))
-    name = Column(ForeignKey('vehicle.name'))
-    cargo_capacity = Column(Integer, nullable=False)
-    created = Column(String, nullable=False)
-    crew = Column(Integer, nullable=False)
-    length = Column(String, nullable=False)
-    manufacturer = Column(String, nullable=False)
-    max_atmosphering_speed = Column(Integer, nullable=False)
-    model = Column(String, nullable=False)
-    vehicle_class = Column(String, nullable=False)
-    passengers = Column(Integer, nullable=False)
-    pilots = Column(String, nullable=False)
-    films = Column(String, nullable=False)
-    url = Column(String, nullable=False)
-    created=Column(DateTime, nullable=False)
-    edited= Column(DateTime, nullable=False)
-    vehicle_properties = relationship('Vehicle', populates="Vehicle_Properties" , lazy=True)  
-
-class Character(Base):
+class Character(db.Model):
     __table_name__ = 'character'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    url = Column(String)
-    favorites = relationship('Favorites', backref='character', lazy=True)
-    character_properties = relationship('Character_Properties', populates='character', lazy=True)
-
-class Character_Properties(Base):
-    __table_name__ = "character_properties"
-    id = Column(Integer, primary_key=True)
-    character_id = Column(ForeignKey('Character.id'))
-    cargo_capacity = Column(String, nullable=False)
-    consumables = Column(String, nullable=False)
-    crew = Column(String, nullable=False)
-    cargo_manufacturercapacity = Column(String, nullable=False)
-    max_atmosphering_speed = Column(String, nullable=False)
-    pilots = Column(String, nullable=False)
-    passengers = Column(String, nullable=False)
-    films = Column(String, nullable=False)
-    vehicle_class = Column(String, nullable=False)
-    url = Column(String, nullable=False)
-    created = Column(DateTime)
-    edited = Column(DateTime)
-    character = relationship('Character', populates='character_properties', lazy=True)
-
-## Draw from SQLAlchemy base
-render_er(Base, "diagram.png")
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    cargo_capacity = db.Column(db.String, nullable=False)
+    consumables = db.Column(db.String, nullable=False)
+    crew = db.Column(db.String, nullable=False)
+    cargo_manufacturercapacity = db.Column(db.String, nullable=False)
+    max_atmosphering_speed = db.Column(db.String, nullable=False)
+    pilots = db.Column(db.String, nullable=False)
+    passengers = db.Column(db.String, nullable=False)
+    films = db.Column(db.String, nullable=False)
+    vehicle_class = db.Column(db.String, nullable=False)
+    url = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime)
+    edited = db.Column(db.DateTime)
+    favorites = db.relationship('Favorites', backref='character', lazy=True)
+    
